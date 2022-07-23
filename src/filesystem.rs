23b1952706d02d8c0bc6bab59f520329c9458e89
Sumiishi Aran <57449457+s-aran@users.noreply.github.com>
2022-07-24 02:19:52 +0900

    implemented filename to ID or from ID to filename fn

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -27,24 +27,24 @@ impl FileSystem {
   /// * `target` - The target directory.
   /// * `recursive` - If true, enumerate files recursively.
   /// * `callback` - The callback function.
-  /// 
+  ///
   /// # Returns
   /// * `Ok(())` - If the operation succeeded.
   /// * `Err(e)` - If the operation failed.
-  /// 
+  ///
   /// # Examples
   /// ```
   /// use std::path::Path;
   /// use nae::filesystem::FileSystem;
-  /// 
+  ///
   /// let target = Path::new("./");
   /// let mut callback = |path: &Path| {
   ///  println!("{}", path.to_str().unwrap());    // Prints the file name. (e.g. "./README.md")
   /// };
-  /// 
+  ///
   /// FileSystem::enum_files(&target, false, &mut callback).unwrap();
   /// ```
-  /// 
+  ///
   pub fn enum_files(
     path: &Path,
     recursive: bool,
@@ -71,12 +71,201 @@ impl FileSystem {
 
     Ok(())
   }
+
+  #[cfg(target_os = "windows")]
+  pub fn get_id_by_filename(path: &Path) -> Result<String, String> {
+    use std::{
+      mem::size_of_val,
+      os::{raw::c_void, windows::prelude::OsStrExt},
+    };
+
+    use windows_sys::Win32::{
+      Foundation::{CloseHandle, GetLastError, HANDLE, INVALID_HANDLE_VALUE},
+      Storage::FileSystem::{
+        CreateFileW, FileIdInfo, GetFileInformationByHandleEx, FILE_ATTRIBUTE_NORMAL,
+        FILE_GENERIC_READ, FILE_ID_128, FILE_ID_INFO, FILE_SHARE_READ, OPEN_EXISTING,
+      },
+    };
+
+    let mut wstr: Vec<u16> = path.as_os_str().encode_wide().collect();
+    // terminal character
+    wstr.push(0x0000);
+
+    let h_file: HANDLE = unsafe {
+      CreateFileW(
+        wstr.as_ptr(),
+        FILE_GENERIC_READ,
+        FILE_SHARE_READ,
+        std::ptr::null(),
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        0,
+      )
+    };
+
+    if h_file == INVALID_HANDLE_VALUE {
+      return Err("file open failed.".to_string());
+    }
+
+    let mut id: FILE_ID_INFO = FILE_ID_INFO {
+      VolumeSerialNumber: 0,
+      FileId: FILE_ID_128 {
+        Identifier: Default::default(),
+      },
+    };
+
+    // GetFileInformationByHandleEx() returned FALSE(=0)?
+    if unsafe {
+      GetFileInformationByHandleEx(
+        h_file,
+        FileIdInfo,
+        &mut id as *mut _ as *mut c_void,
+        size_of_val(&id).try_into().unwrap(),
+      )
+    } == 0
+    {
+      return Err(format!(
+        "GetFileInformationByHandleEx() failed. code: {}",
+        unsafe { GetLastError() }
+      ));
+    }
+
+    unsafe { CloseHandle(h_file) };
+
+    // [u8; 16] -> String
+    let mut id_str = String::new();
+    for i in id.FileId.Identifier.iter().rev() {
+      id_str += format!("{:02X}", i).as_str();
+    }
+
+    Ok(id_str)
+  }
+
+  #[cfg(target_os = "windows")]
+  pub fn get_filename_by_id(id_str: &String, hint_dir: &Path) -> Result<String, String> {
+    use std::{mem, os::windows::prelude::OsStrExt};
+
+    use windows_sys::Win32::{
+      Foundation::{CloseHandle, GetLastError, HANDLE, INVALID_HANDLE_VALUE, NO_ERROR},
+      Storage::FileSystem::{
+        CreateFileW, GetFinalPathNameByHandleW, OpenFileById, FILE_FLAG_BACKUP_SEMANTICS,
+        FILE_ID_128, FILE_ID_DESCRIPTOR, FILE_NAME_NORMALIZED, FILE_READ_ATTRIBUTES,
+        FILE_SHARE_READ, OPEN_EXISTING, READ_CONTROL, SYNCHRONIZE,
+      },
+      System::WindowsProgramming::VOLUME_NAME_DOS,
+    };
+
+    let mut wstr: Vec<u16> = hint_dir.as_os_str().encode_wide().collect();
+    // terminal character
+    wstr.push(0x0000);
+
+    // open hint directory for OpenFileById()
+    let h_dir_hint: HANDLE = unsafe {
+      CreateFileW(
+        wstr.as_ptr(),
+        READ_CONTROL,
+        FILE_SHARE_READ,
+        std::ptr::null(),
+        OPEN_EXISTING,
+        FILE_FLAG_BACKUP_SEMANTICS,
+        0,
+      )
+    };
+
+    if h_dir_hint == INVALID_HANDLE_VALUE {
+      return Err(format!("drive open failed. code: {}", unsafe { GetLastError() }).to_string());
+    }
+
+    let mut id = FILE_ID_128 {
+      Identifier: Default::default(),
+    };
+
+    // String -> [u8;  16]
+    for (i, c1) in id_str.chars().rev().enumerate() {
+      // odd check
+      if i & 0x1 == 1 {
+        let c2 = id_str.chars().rev().nth(i - 1).unwrap();
+        // '0' -> 0
+        id.Identifier[i >> 1] = ((c1 as u8 - '0' as u8) << 4) | (c2 as u8 - '0' as u8);
+      }
+    }
+
+    let id_descriptor = FILE_ID_DESCRIPTOR {
+      dwSize: mem::size_of::<FILE_ID_DESCRIPTOR>() as u32,
+      Type: 2,
+      Anonymous: windows_sys::Win32::Storage::FileSystem::FILE_ID_DESCRIPTOR_0 {
+        ExtendedFileId: id,
+      },
+    };
+
+    let h_file = unsafe {
+      OpenFileById(
+        h_dir_hint,
+        &id_descriptor,
+        SYNCHRONIZE | FILE_READ_ATTRIBUTES,
+        FILE_SHARE_READ,
+        std::ptr::null(),
+        0,
+      )
+    };
+
+    if h_file == INVALID_HANDLE_VALUE {
+      return Err(
+        format!("directory open failed. code: {}", unsafe { GetLastError() }).to_string(),
+      );
+    }
+
+    unsafe { CloseHandle(h_dir_hint) };
+
+    let mut name = vec![0u16; 512];
+    let length = unsafe {
+      GetFinalPathNameByHandleW(
+        h_file,
+        name.as_mut_ptr(),
+        name.capacity() as u32,
+        FILE_NAME_NORMALIZED | VOLUME_NAME_DOS,
+      )
+    };
+
+    if unsafe { GetLastError() } != NO_ERROR {
+      let n = unsafe { GetLastError() };
+      return Err(format!("GetFinalPathNameByHandleW() failed. code: {}", n));
+    }
+
+    unsafe { CloseHandle(h_file) };
+
+    // drop '\0' elements
+    name.truncate(length as usize);
+    let name_str = String::from_utf16(&name).unwrap();
+    let path = Path::new(&name_str);
+
+    Ok(
+      path
+        .file_name()
+        .unwrap_or_default()
+        .to_string_lossy()
+        .to_string(),
+    )
+  }
+
+  #[cfg(target_os = "linux")]
+  pub fn get_id_by_filename(path: &Path) -> Result<String, String> {
+    Ok("todo")
+  }
 }
 
 #[cfg(test)]
 mod tests {
+
+  use windows_sys::Win32::Foundation::{
+    CloseHandle, GetLastError, HANDLE, INVALID_HANDLE_VALUE, NO_ERROR,
+  };
+  use windows_sys::Win32::Storage::FileSystem::{
+    CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING,
+  };
+
   use crate::filesystem::FileSystem;
-  use std::{fs::File, io::Write, path::Path};
+  use std::{fs::File, io::Write, os::windows::prelude::OsStrExt, path::Path};
 
   fn create_file(path: &Path) -> bool {
     let mut file = match File::create(path) {
@@ -179,5 +368,13 @@ mod tests {
     assert!(exists_file(Path::new(invalid_dir)));
     assert!(FileSystem::enum_files(Path::new(invalid_dir), false, &mut func).is_err());
     assert!(!called);
+  }
+
+  #[test]
+  fn test_file_id() {
+    let filename = "README.md";
+    let id = FileSystem::get_id_by_filename(Path::new(filename));
+    let actual_filename = FileSystem::get_filename_by_id(&id.unwrap(), Path::new("."));
+    assert_eq!(filename, actual_filename.unwrap());
   }
 }
